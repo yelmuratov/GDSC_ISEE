0178464c5aa7c2a1fbf46c6f70b13fb4aec4cc3a
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-02-20 18:02:58 +0500

    Merge branch 'main' of https://github.com/yelmuratov/GDSC_ISEE

--- gdsc_isee/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gdsc_isee/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
